6e698fac66992f7c31ddcdf350fa1ffc3e7b145f
Akinade Mayowa <akinade.mayowa@gmail.com>
2023-03-29 16:41:04 +0100

    added post details comments page

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,8 +9,11 @@ import './Home.css';
 
 const Post = ({ post, sub }) => {
   const [comment, setComment] = useState('');
+  const [comment2, setComment2] = useState('');
   const [postId, setPostId] = useState({});
   const [postComment, setPostComment] = useState([]);
+  const [show, setShow] = useState(false);
+  const [item, setItem] = useState([]);
   const notifyError = (msg) => toast.error(msg);
   const notifySuccess = (msg) => toast.success(msg);
   const navigate = useNavigate();
@@ -32,12 +35,17 @@ const Post = ({ post, sub }) => {
     getComments(postId);
   }, [postComment, postId]);
 
-  const toggleComment = (posts) => {
-    console.log(posts);
+  const toggleComment = (post) => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+      setItem(post);
+    }
   };
 
   // save new comments
-  const makeComment = async (post, text, id) => {
+  const makeComment = async (text, id) => {
     const commentParams = {
       input: { text: text, postID: id, userID: sub },
     };
@@ -52,43 +60,44 @@ const Post = ({ post, sub }) => {
   };
 
   return (
-    <div className="card" key={post.id}>
-      {/* card header */}
-      <div className="card-header">
-        <div className="card-pic">
-          <img src={user1} alt="" />
+    <>
+      <div className="card" key={post.id}>
+        {/* card header */}
+        <div className="card-header">
+          <div className="card-pic">
+            <img src={user1} alt="" />
+          </div>
+          <h5>{post.username}</h5>
         </div>
-        <h5>{post.username}</h5>
-      </div>
-      {/* card image */}
-      <div className="card-image">
-        <img src={post.photo} alt="" />
-      </div>
-      <p>{post.body}</p>
-      {postComment.length === 1 ? (
-        <p
-          style={{ cursor: 'pointer', fontWeight: 'bold' }}
-          onClick={() => toggleComment(post)}
-        >
-          View {postComment.length} comment
-        </p>
-      ) : postComment.length === 0 ? (
-        <p
-          style={{ cursaor: 'pointer', fontWeight: 'bold' }}
-          onClick={() => toggleComment(post)}
-        >
-          Be the first to comment
-        </p>
-      ) : (
-        <p
-          style={{ cursor: 'pointer', fontWeight: 'bold' }}
-          onClick={() => toggleComment(post)}
-        >
-          View all {postComment.length} comments
-        </p>
-      )}
-      {/* card content */}
-      {/* <div className="card-content">
+        {/* card image */}
+        <div className="card-image">
+          <img src={post.photo} alt="" />
+        </div>
+        <p>{post.body}</p>
+        {postComment.length === 1 ? (
+          <p
+            style={{ cursor: 'pointer', fontWeight: 'bold' }}
+            onClick={() => toggleComment(post)}
+          >
+            View {postComment.length} comment
+          </p>
+        ) : postComment.length === 0 ? (
+          <p
+            style={{ cursaor: 'pointer', fontWeight: 'bold' }}
+            onClick={() => toggleComment(post)}
+          >
+            Be the first to comment
+          </p>
+        ) : (
+          <p
+            style={{ cursor: 'pointer', fontWeight: 'bold' }}
+            onClick={() => toggleComment(post)}
+          >
+            View all {postComment.length} comments
+          </p>
+        )}
+        {/* card content */}
+        {/* <div className="card-content">
               {post.likes.includes(
                 JSON.parse(localStorage.getItem('user'))._id
               ) ? (
@@ -154,23 +163,104 @@ const Post = ({ post, sub }) => {
               )}
             </div> */}
 
-      {/* comment */}
-      <div className="add-comment">
-        <span className="material-symbols-outlined">mood</span>
-        <input
-          type="text"
-          placeholder="Add a comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <button
-          className="comment"
-          onClick={() => makeComment(post, comment, post.id)}
-        >
-          Reply
-        </button>
+        {/* comment */}
+        <div className="add-comment">
+          <span className="material-symbols-outlined">mood</span>
+          <input
+            type="text"
+            placeholder="Add a comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            className="comment"
+            onClick={() => makeComment(comment, post.id)}
+          >
+            Reply
+          </button>
+        </div>
       </div>
-    </div>
+
+      {/* Shiw comments */}
+
+      {show && (
+        <div className="showComment">
+          <div className="container">
+            <div className="postPic">
+              <img src={item.photo} alt="" />
+            </div>
+            <div className="details">
+              {/* card header */}
+              <div
+                className="card-header"
+                style={{ borderBottom: '1px solid #00000029' }}
+              >
+                <div className="card-pic">
+                  <img src={user1} alt="" />
+                </div>
+                <h5>
+                  {item.firstName} {item.lastName}
+                </h5>
+              </div>
+              {/* comment section */}
+              <div
+                className="comment-section"
+                style={{ borderBottom: '1px solid #00000029' }}
+              >
+                {item.comments.map((comment, index) => {
+                  return (
+                    <p className="comm" key={index}>
+                      <span
+                        className="commenter"
+                        style={{ fontWeight: 'bolder' }}
+                      >
+                        {comment.postedBy.name}{' '}
+                      </span>
+                      <span className="commentText">{comment.comment}</span>
+                    </p>
+                  );
+                })}
+              </div>
+
+              {/* card content */}
+              <div className="card-content">
+                {item.likes.length === 1 ? (
+                  <p>{item.likes.length} Like</p>
+                ) : (
+                  <p>{item.likes.length} Likes</p>
+                )}
+
+                <p>{item.body}</p>
+              </div>
+              {/* comment */}
+              <div className="add-comment">
+                <span className="material-symbols-outlined">mood</span>
+                <input
+                  type="text"
+                  placeholder="Add a comment"
+                  value={comment2}
+                  onChange={(e) => setComment2(e.target.value)}
+                />
+                <button
+                  className="comment"
+                  onClick={() => {
+                    makeComment(comment2, item.id);
+                    toggleComment();
+                  }}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="close-comment" onClick={() => toggleComment()}>
+            <span className="material-symbols-outlined material-symbols-outlined-comment">
+              close
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
